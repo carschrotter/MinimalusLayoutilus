bdd7475760c3fb2ecab3e707f3795d2ca3ae7463
MNHcC (Michael Hegenbarth) <carschrotter@gmail.com>
2014-01-25 23:41:52 +0100

    change to git submodule

--- initial.php
+++ initial.php
@@ -6,7 +6,7 @@
 
 namespace mnhcc\ml;
 
-if(!\defined("INDEX")) die();
+if(!\defined("INDEX")) die('Define constant "INDEX" befor run Programm!');
 
 use mnhcc\ml\classes as classes; {
     define('STARTTIME', \microtime(true));
@@ -19,9 +19,7 @@ use mnhcc\ml\classes as classes; {
     session_start();
     ini_set("display_errors", false);
     
-    if (!defined('MNHCC_PATH') && !defined('ROOT_PATH') ) {
-	die('Define constant "NHCC_PATH" and "ROOT_PATH"!' );
-    }
+  
     /**
      * NameSpace Seperator
      */
@@ -33,10 +31,16 @@ use mnhcc\ml\classes as classes; {
     define("br", "<br />".n);
     define("php", ".php");
     define("DS", DIRECTORY_SEPARATOR);
-
-    if (file_exists(ROOT_PATH.DS.'load.php')) {
-        $config = include ROOT_PATH.DS.'load.php';
-	$config = is_scalar($config) ? [] : $config;
+    
+    if (!defined('MNHCC_PATH')) define("ROOT_PATH", __DIR__);
+    if (!defined('ROOT_PATH')) define("NHCC_PATH", __DIR__);
+    
+    //die('Define constant "NHCC_PATH" and "ROOT_PATH"!' );
+    foreach (['load', 'config'] as $filename) {
+	if (file_exists(ROOT_PATH.DS.$filename.'.php')) {
+	    $config = include ROOT_PATH.DS.$filename.'.php';
+	    $config = is_scalar($config) ? [] : $config;
+	}
     }
     if (file_exists(ROOT_PATH.DS.'config.json')) {
         $config = array_merge((array) $config, (array)  json_decode(file_get_contents(ROOT_PATH.DS.'config.json'))) ;
@@ -60,19 +64,10 @@ use mnhcc\ml\classes as classes; {
             classes\ClassHandler::addRootNamespace('Config');
     
     define('CONFIG', $ConfigClass);
-    classes\ClassHandler::setDefineLocation(CONFIG, ROOT_PATH . DS . 'Config' . php);
+    classes\ClassHandler::setDefineLocation(CONFIG, ROOT_PATH . DS . 'Config.class' . php);
     classes\ClassHandler::Load(__NAMESPACE__."\\classes\\exception\\Exception");
-    
-    //classes\ClassHandler::addDependence('interface', 'FOOBAR');
-    //classes\ClassHandler::addDependence('extension', 'PHPEXTENSION');
-    /**
-     * initial the config
-     * @global Config $config
-     */
-    
-
     $config->paths['root'] = ROOT_PATH;
-    classes\ClassHandler::loadLibrary('FirePHP');
+
     //classes\ClassHandler::Load(classes\ClassHandler::addRootNamespace('Kint') );
     //classes\Helper::setFunction((new classes\ReflectionStaticMethod(classes\ClassHandler::addRootNamespace('Kint'), 'dump')), 'dump');
     //classes\Kint::defaultModifier('@');
